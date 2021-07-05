b38a0ca426eab08c730f137ec8456a22f121625a
osadasami <osadasami@gmail.com>
2021-07-05 09:39:44 +0500

    add spanish greeting

--- 001-hello-world/hello.go
+++ 001-hello-world/hello.go
@@ -2,17 +2,23 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello(""))
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("", ""))
+	fmt.Println(Hello("Chris", ""))
 }
--- 001-hello-world/hello_test.go
+++ 001-hello-world/hello_test.go
@@ -12,16 +12,22 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("Hello with name", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assetsCorrectMessage(t, got, want)
 	})
 
 	t.Run("Hello without name", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assetsCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assetsCorrectMessage(t, got, want)
 	})
 }
