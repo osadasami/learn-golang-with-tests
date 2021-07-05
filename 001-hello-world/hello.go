bdd9c02b944749702e8926caf10aad06bbbd8faa
osadasami <osadasami@gmail.com>
2021-07-05 09:18:59 +0500

    hello without name and with many names

--- 001-hello-world/hello.go
+++ 001-hello-world/hello.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const englishHelloPrefix = "Hello, "
+
+func Hello(names ...string) string {
+	var name string
+
+	if len(names) == 0 {
+		name = "World"
+	} else {
+		name = names[0]
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello())
+	fmt.Println(Hello("Chris"))
 }
--- 001-hello-world/hello_test.go
+++ 001-hello-world/hello_test.go
@@ -3,10 +3,33 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assetsCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Hello with name", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		assetsCorrectMessage(t, got, want)
+	})
+
+	t.Run("Hello without name", func(t *testing.T) {
+		got := Hello()
+		want := "Hello, World"
+
+		assetsCorrectMessage(t, got, want)
+	})
+
+	t.Run("Hello with many names use only first name", func(t *testing.T) {
+		got := Hello("Chris", "John")
+		want := "Hello, Chris"
+
+		assetsCorrectMessage(t, got, want)
+	})
+
 }
