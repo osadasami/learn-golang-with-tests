82199ad1aeecda50428fa54e4c1b49ef6c627771
osadasami <osadasami@gmail.com>
2021-07-07 15:11:28 +0500

    can specify repeat times

--- 003-iteration/main_test.go
+++ 003-iteration/main_test.go
@@ -3,10 +3,16 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	got := Repeat("a")
-	want := "aaaaa"
+	got := Repeat("a", 3)
+	want := "aaa"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 3)
 	}
 }
