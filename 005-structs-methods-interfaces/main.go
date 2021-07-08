7823f4a651ca40b589b5c3d492db0fa3956f128e
osadasami <osadasami@gmail.com>
2021-07-08 18:29:43 +0500

    test Circle area

--- 005-structs-methods-interfaces/main.go
+++ 005-structs-methods-interfaces/main.go
@@ -1,5 +1,7 @@
 package structsmethodsinterfaces
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -15,4 +17,8 @@ func (r Rectangle) Perimeter() float64 {
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- 005-structs-methods-interfaces/main_test.go
+++ 005-structs-methods-interfaces/main_test.go
@@ -22,4 +22,14 @@ func TestArea(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
