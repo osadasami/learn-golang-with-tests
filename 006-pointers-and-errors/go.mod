3399b5197dd75486d2be09368f618ee95a8a8895
osadasami <osadasami@gmail.com>
2021-10-15 13:11:44 +0500

    test balance bitcoin

---
+++ 006-pointers-and-errors/go.mod
@@ -0,0 +1,11 @@
+module _06_pointers_and_errors
+
+go 1.17
+
+require (
+	github.com/kisielk/errcheck v1.6.0 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
---
+++ 006-pointers-and-errors/main.go
@@ -0,0 +1,39 @@
+package _06_pointers_and_errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
