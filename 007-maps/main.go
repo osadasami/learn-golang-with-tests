3b4ec42331417ade997b2d0774d98772c93ae5e3
osadasami <osadasami@gmail.com>
2021-11-03 17:22:53 +0500

    can update word definition in the dictionary

--- 007-maps/main.go
+++ 007-maps/main.go
@@ -7,7 +7,14 @@ type Dictionary map[string]string
 var (
 	ErrNotFound   = errors.New("could not find the word you were looking for")
 	ErrWordExists = errors.New("cannot add word because it already exists")
+	ErrWordDoesNotExist = errors.New("cannot update word because it does not exist")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -26,6 +33,21 @@ func (d Dictionary) Add(word, definition string) error {
 		d[word] = definition
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
 	default:
 		return err
 	}
